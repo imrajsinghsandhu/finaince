8cffb7a38ae2a27fa45d46666e897c33ce988836
imraj singh sandhu <e0543951@u.nus.edu>
2024-03-03 01:26:47 +0800

    Add function to randomise a p tag colour
    add simple button and body

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const randomiseTextColour = () => {
+    // i want to change the colour of the button background
+    const p = document.querySelector('p');
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    p.style.color = randomColor;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <button onClick={() => randomiseTextColour()}>Home</button>
+        </nav>
       </header>
+      <main>
+        <p>Putting the AI in finaince</p>
+      </main>
+      <footer>
+        <p>
+          I am bringing sophisticated Machine Learning indicators to the retail investor.
+          I am levelling out the playing field
+        </p>
+      </footer>
     </div>
   );
 }
